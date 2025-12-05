7bde564ec258e4aa3d2ec6ba0598736cbd4f1cfa
Felix Gishu <nclips254@gmail.com>
2025-12-05 23:22:06 +0300

    Added the media url

--- cfs_farmers_platform/urls.py
+++ cfs_farmers_platform/urls.py
@@ -12,3 +12,9 @@ urlpatterns = [
     path('core/', include('core.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'), # Homepage
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
