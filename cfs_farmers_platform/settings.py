448f49bde9303034507d0ad9a8ccfae11e593ee5
Felix Gishu <nclips254@gmail.com>
2025-12-05 19:51:04 +0300

    added core services

--- cfs_farmers_platform/settings.py
+++ cfs_farmers_platform/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',  # Custom users app
+    'core',   # Core services and utilities
 ]
 
 MIDDLEWARE = [
