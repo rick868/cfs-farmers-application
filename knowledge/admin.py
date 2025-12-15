c254bca0c40efa01c355cc42be1337ed3bff80b7
Felix Gishu <nclips254@gmail.com>
2025-12-15 14:26:31 +0300

    Added  policy documents on the admin side

--- knowledge/admin.py
+++ knowledge/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PolicyDocument
+
+
+@admin.register(PolicyDocument)
+class PolicyDocumentAdmin(admin.ModelAdmin):
+    list_display = ("title", "region", "category", "created_at")
+    list_filter = ("region", "category", "created_at")
+    search_fields = ("title", "region", "category")
+    ordering = ("-created_at",)
